4f995e50938059e05d3aa87aee8ea024bad88279
hashim <hashimmoochikkalckra@gmail.com>
2021-12-06 19:10:16 +0530

    second commit

--- src/components/SimpleForm.js
+++ src/components/SimpleForm.js
@@ -1,17 +1,29 @@
-import React from 'react'
+import React from 'react';
+import { useFormik } from 'formik';
 
 function SimpleForm() {
+
+    const formik = useFormik({
+        initialValues: {
+            name: "",
+            email: "",
+            password: ""
+        }
+    })
+
+    console.log('Form Values', formik.values)
+
     return (
         <div>
             <form>
                 <label htmlFor='name'>Name</label>
-                <input type='text' id='name' name='name' />
+                <input type='text' id='name' name='name' onChange={formik.handleChange} value={formik.values.name} />
 
                 <label htmlFor='email'>E-mail</label>
-                <input type='email' id='email' name='email' />
+                <input type='email' id='email' name='email' onChange={formik.handleChange} value={formik.values.email} />
 
                 <label htmlFor='password'>Password</label>
-                <input type='text' id='password' name='password' />
+                <input type='text' id='password' name='password' onChange={formik.handleChange} value={formik.values.password} />
 
                 <button>Submit</button>
             </form>
